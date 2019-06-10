938c8e9babcde41eb3be2144acbff33a0a5fc011
github username <github email address>
2019-06-10 18:48:15 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  resources :assistants, only: [:index, :show, :create]
+  resources :yogaclasses
+  resources :subrequests
+
+  #one of these will be nested-- get ready for thata
+
+  #will need the facebook authorization with this as well
+  root 'welcome#home'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  post '/logout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
