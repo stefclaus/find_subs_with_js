b398243e1f3eebef0ab65150cf7f9b83a705c389
github username <github email address>
2019-06-10 18:39:27 -0400

    add assistant controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :current_assistant
+
+ def current_assistant
+   @assistant = (Assistant.find_by(id: session[:user_id]) || Assistant.new)
+ end
+
+ def logged_in?
+   current_assistant.id != nil
+ end
+
+ def require_logged_in
+   if logged_in?
+   else
+     redirect_to(controller: 'sessions', action: 'new')
+   end
+ end
+
 end
---
+++ app/controllers/assistants_controller.rb
@@ -0,0 +1,32 @@
+class AssistantsController < ApplicationController
+
+  def new
+    @assistant = Assistant.new
+  end
+
+  def create
+    @assistant = Assistant.create(assistant_params)
+   # return redirect_to root_path unless @assistant.save
+    session[:assistant_id] = @assistant.id
+    redirect_to assistant_path(@assistant)
+  end
+
+  def show
+    if !logged_in?
+      redirect_to "/"
+    else
+      @assistant = Assistant.find_by(id:params[:id])
+    end
+	end
+
+
+
+
+  private
+
+  def assistant_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+
+end
