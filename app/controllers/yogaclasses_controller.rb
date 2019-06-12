ea825f34b4056ae381734c85579c7243659e96d2
github username <github email address>
2019-06-12 14:38:23 -0400

    add link to yogaclass index

--- app/controllers/yogaclasses_controller.rb
+++ app/controllers/yogaclasses_controller.rb
@@ -4,4 +4,10 @@ class YogaclassesController < ApplicationController
     @yogaclasses = Yogaclass.all
   end
 
+  def show
+    @yogaclass = Yogaclass.find(params[:id])
+   #@ride = @attraction.rides.build(user_id:current_user.id)
+  end
+
+
 end
