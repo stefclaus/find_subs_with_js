994c6f4ebc2de1ec7c06e30270e94c0c2767867e
github username <github email address>
2019-06-19 16:48:16 -0400

    require all fields on new subrequests view

--- app/models/subrequest.rb
+++ app/models/subrequest.rb
@@ -1,6 +1,7 @@
 class Subrequest < ApplicationRecord
   belongs_to :assistant
 	belongs_to :yogaclass
+  validates :comment, :date, :assistant_covering, :assistant_requesting, :yogaclass_id, presence: true
 
 #  scope :is_in_future, -> (date){ where("date = ?", date > today) }
 # scope :today, lambda { WHERE('DATE(created_at) = ?', Date.today)}
