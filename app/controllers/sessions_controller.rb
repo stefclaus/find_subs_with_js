783ebd2844fc74ffad50ec65b2e78132bb437331
github username <github email address>
2019-06-19 20:58:01 -0400

    add flash message to view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,9 +12,10 @@ class SessionsController < ApplicationController
       redirect_to assistant_path(@assistant)
     else
       flash[:message] = "Sorry, please try again"
-      redirect_to login_path
+      redirect_to '/signin'
   #  @assistant = @assistant.try(:authenticate, params[:assistant][:password])
 #    return redirect_to root_path unless @assistant
+    end
   end
 
 
