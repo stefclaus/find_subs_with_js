1dd2a31af9735523bc1bdf1fc9e4ed975ca89926
github username <github email address>
2019-08-09 10:55:38 -0400

    add methods to yogaclass controller

--- app/controllers/yogaclasses_controller.rb
+++ app/controllers/yogaclasses_controller.rb
@@ -3,12 +3,27 @@ class YogaclassesController < ApplicationController
 
   def index
     @yogaclasses = Yogaclass.all
-    
+
   end
 
   def show
     @yogaclass = Yogaclass.find(params[:id])
    #@ride = @attraction.rides.build(user_id:current_user.id)
+  end
+
+  def instructor
+    yogaclass = Yogaclass.find(params[:id])
+    render plain: yogaclass.instructor
+  end
+
+  def time
+    yogaclass = Yogaclass.find(params[:id])
+    render plain: yogaclass.time
+  end
+
+  def weekday
+    yogaclass = Yogaclass.find(params[:id])
+    render plain: yogaclass.weekday
   end
 
   def subrequest
