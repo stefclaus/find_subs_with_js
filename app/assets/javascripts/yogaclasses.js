94ad1ecf2a35c9a6e47997e2db8cd64dbce6a786
github username <github email address>
2019-08-12 21:57:11 -0400

    add links to yoga classes on index page

--- app/assets/javascripts/yogaclasses.js
+++ app/assets/javascripts/yogaclasses.js
@@ -6,8 +6,29 @@ $(() => {
 const bindingClickHandlers = () => {
   $('.all-yogaclasses').on('click', (e) => {
     e.preventDefault()
+    history.pushState(null, null, "yogaclasses")
     fetch(`/yogaclasses.json`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(yogaclasses => {
+      $('#app-container').html('')
+      yogaclasses.forEach(yogaclass => {
+        let newYogaclass = new Yogaclass(yogaclass)
+        let yogaclassHtml = newYogaclass.formatIndex()
+        $('#app-container').append(yogaclassHtml)
+      })
+    })
   })
+}
+
+function Yogaclass(yogaclass) {
+  this.id = yogaclass.id
+  this.name = yogaclass.name
+  this.weekday = yogaclass.weekday
+  this.instructor = yogaclass. instructor
+}
+
+Yogaclass.prototype.formatIndex = function(){
+  let yogaclassHtml = `
+  <a hfref="/yogaclasses/${this.id}"><h1>${this.name}</h1>`
+  return yogaclassHtml
 }
